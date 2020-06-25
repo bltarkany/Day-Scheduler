fd5ca507289e8b0aa598245fe529c0084e9fcb98
Bobbi Tarkany <bl.tarkany@gmail.com>
2020-06-24 19:07:31 -0700

    created on click button for each save button.

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 // when the DOM is fully loaded start this
 $(document).ready(function() {
-    
+
+    // create onclick event for the saveBtn
+    $('.saveBtn').on('click', function() {
+        // grab the sibling of the saveBtn because both are children of time-block div/class
+        var value = $(this).siblings('.description').val();
+        console.log(value);
+        // grab the time slot it is in based on time-block ID
+        var time = $(this).parent().attr('id');
+        console.log(time);
+
+        // print to local storage the time slot and the activity 
+        // setItem(key, value)
+        localStorage.setItem(time, value);
+    });
 
 });
